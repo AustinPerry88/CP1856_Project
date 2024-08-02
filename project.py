9b49d618bc7cd882cadbabdb7f59bb4821955aab
Shangyunw <shangyun.wang23@ed.cna.nl.ca>
2024-08-02 13:00:40 -0230

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+import csv
+
 def display_menu():
     print("Welcome to the Video Library Management System!")
     print("Choose from the options below:\n")
@@ -8,3 +10,33 @@ def display_menu():
     print("5. Display detailed video information.")
     print("6. List videos by criteria.")
     print("7. Exit.\n")
+
+def save_all_videos(videos):
+    with open("movielist.csv", 'w') as file:
+        for video in videos:
+            title = video[0]
+            director = video[1]
+            release_year = video[2]
+            genre = video[3]
+            duration = video[4]
+            file.write(f"{title}#{director}#{release_year}#{genre}#{duration}\n")
+
+
+def get_all_videos():
+    videos = []
+    try:
+        with open("movielist.csv", 'r') as file:
+            for line in file:
+                s = line.split("#")
+                title = s[0]
+                director = s[1]
+                release_year = int(s[2])
+                genre = s[3]
+                duration = int(s[4])
+                videos.append([title, director, release_year, genre, duration])
+        return videos
+    except FileNotFoundError:
+        return videos
+
+
+
