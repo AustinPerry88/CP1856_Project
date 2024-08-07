a46350051df11d86aad13db3e3ae533f5b0ebcd8
Shangyunw <shangyun.wang23@ed.cna.nl.ca>
2024-08-07 14:06:14 -0230

    Update edit_video.py

--- edit_video.py
+++ edit_video.py
@@ -5,23 +5,27 @@ Created on Fri Aug  2 15:39:28 2024
 @author: shang
 """
 
-from ReadWrite import get_all_videos, save_all_videos
+import ReadWrite
 import display_videos
 
 def edit_video():
-    videos = get_all_videos()
+    """
+   Edits a video from the video list based on the inputted index.
+
+   """
+    video_list = ReadWrite.read_file()
     display_videos.display_videos()
     index = int(input("Enter the index of the video to edit: ")) - 1
 
-    if 0 <= index < len(videos):
-        new_title = input(f"Enter new title ({videos[index][0]}): ") or videos[index][0]
-        new_director = input(f"Enter new director ({videos[index][1]}): ") or videos[index][1]
-        new_release_year = input(f"Enter new release year ({videos[index][2]}): ") or videos[index][2]
-        new_genre = input(f"Enter new genre ({videos[index][3]}): ") or videos[index][3]
-        new_duration = input(f"Enter new duration ({videos[index][4]}): ") or videos[index][4]
+    if 0 <= index < len(video_list):
+        new_title = input(f"Enter new title ({video_list[index][0]}): ") or video_list[index][0]
+        new_director = input(f"Enter new director ({video_list[index][1]}): ") or video_list[index][1]
+        new_release_year = input(f"Enter new release year ({video_list[index][2]}): ") or video_list[index][2]
+        new_genre = input(f"Enter new genre ({video_list[index][3]}): ") or video_list[index][3]
+        new_duration = input(f"Enter new duration ({video_list[index][4]}): ") or video_list[index][4]
 
-        videos[index] = [new_title, new_director, new_release_year, new_genre, new_duration]
-        save_all_videos(videos)
+        video_list[index] = [new_title, new_director, new_release_year, new_genre, new_duration]
+        ReadWrite.write_file(video_list)
         print("Video edited successfully!")
     else:
         print("Invalid index!")
